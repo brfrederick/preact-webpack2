cc28a43725ec6d37b2d2bc5fad5ec03211172723
Braxton Frederick <bfrederick@constantcontact.com>
2017-02-19 11:13:34 -0500

    Add app and hello world components

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact';
+import Hello from './hello';
+
+export default class App extends Component {
+  render() {
+    <div class="app">
+      <h1>Hello!</h1>
+      <Hello />
+    </div>
+  }
+
+}
